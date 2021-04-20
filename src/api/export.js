11def261abcc151e35b0850e6bc75997080d3b9c
范玉龙 <13067882143@163.com>
2021-04-20 17:52:29 +0800

    全部完成

--- src/api/export.js
+++ src/api/export.js
@@ -1,28 +1,27 @@
 import { MessageBox,Message } from 'element-ui';
-import md5 from 'js-md5';
 
-import {middleWare} from './request.js'
-
-export default{
-	exportUp(url){
-		MessageBox.confirm('确认导出?', '提示', {
-			confirmButtonText: '确定',
-			cancelButtonText: '取消',
-			type: 'warning'
-		}).then(() => {
-			let req = {};
-			let req_arr = url.split('?')[1].split('&');
-			req_arr.map(item => {
-				req[item.split('=')[0]] = item.split('=')[1];
-			})
-      let get_str = middleWare(req,'get');
-      let open_url = `${location.origin}/api/${url.split('?')[0]}?${get_str}`;
-      window.open(open_url);
-    }).catch(() => {
-     Message({
-      type: 'info',
-      message: '取消导出'
-    });          
-   });
-  }
+function formatJson(filterVal, jsonData) {
+	return jsonData.map(v => filterVal.map(j => v[j]))
 }
+export function exportExcel(data_obj) {
+	MessageBox.confirm('确认导出?', '提示', {
+		confirmButtonText: '确定',
+		cancelButtonText: '取消',
+		type: 'warning'
+	}).then(() => {
+		require.ensure([], () => {
+			　const { export_json_to_excel } = require('../vendor/Export2Excel.js')
+			　const tHeader = data_obj.table_title_list;
+			　const filterVal = data_obj.field_name_list;
+			　const list = data_obj.data_list;
+			　const data = formatJson(filterVal,list);
+			　export_json_to_excel(tHeader, data, data_obj.table_title);
+		})
+	}).catch(() => {
+		Message({
+			type: 'info',
+			message: '取消导出'
+		});          
+	});
+}
+
