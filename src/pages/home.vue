fd586be9e28633941d13159c69aa4a7eaacd03ae
danafan <13067882143@163.com>
2023-10-07 15:42:12 +0800

    款式top200指标解释更新

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -121,133 +121,133 @@
 </div>
 </template>
 <style type="text/css">
-.el-menu-vertical-demo{
-	height: 100%;
-	overflow-y: scroll !important;
-}
+	.el-menu-vertical-demo{
+		height: 100%;
+		overflow-y: scroll !important;
+	}
 </style>
 <style lang="less" scoped>
-.box{
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	display:flex;
-	flex-direction: column;
-	.header{
-		height: 120px;
-		background:#fff !important;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		.gxk{
-			margin-left: 20px;
-			font-size: 18px;
-			color: #333;
-			font-weight: 600;
-		}
-		.user_set{
-			border-bottom: 1px solid #ECEFF8;
-			height: 64px;
+	.box{
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		display:flex;
+		flex-direction: column;
+		.header{
+			height: 120px;
+			background:#fff !important;
 			display: flex;
-			align-items: center; 
-			justify-content: flex-end;
-			.line{
-				margin-right: 8px;
-				margin-left: 8px;
-				border-right: 1px solid #363D66;
-				height:13px;
+			align-items: center;
+			justify-content: space-between;
+			.gxk{
+				margin-left: 20px;
+				font-size: 18px;
+				color: #333;
+				font-weight: 600;
 			}
-			.message_box{
-				margin-right: 8px;
-				position: relative;
-				width: 22px;
-				height: 17px;
-				.message_icon{
-					position: absolute;
-					top: 0;
-					left: 0;
+			.user_set{
+				border-bottom: 1px solid #ECEFF8;
+				height: 64px;
+				display: flex;
+				align-items: center; 
+				justify-content: flex-end;
+				.line{
+					margin-right: 8px;
+					margin-left: 8px;
+					border-right: 1px solid #363D66;
+					height:13px;
+				}
+				.message_box{
+					margin-right: 8px;
+					position: relative;
+					width: 22px;
+					height: 17px;
+					.message_icon{
+						position: absolute;
+						top: 0;
+						left: 0;
+						width: 100%;
+						height: 100%;
+					}
+					.num{
+						position: absolute;
+						top: -10px;
+						right: -18px;
+						border-radius: 8px;
+						background: red;
+						width: 24px;
+						text-align: center;
+						height: 16px;
+						line-height: 16px;
+						font-size: 8px;
+						color: #fff;
+					}
+				}
+				.admin_logo{
+					margin-right: 8px;
+					width: 21px;
+					height: 21px;
+				}
+				.header_text{
+					font-size: 14px;
+					color: #000;
+				}
+			}
+		}
+		.is-active {
+			background:#008DFF !important;
+		}
+		.el-menu{
+			border: none !important;
+		}
+		.content_box{
+			height: 600px;
+			.left_box{
+				background: #363D66;
+				.dd{
+					height: 35px;
+					display: flex;
+					align-items: flex-end;
+					justify-content: flex-end;
+					padding-right: 18px;
+					.collapse_icon{
+						color: #fff;
+						font-size:20px;
+					}
+				}
+				.start_dd{
+					justify-content: flex-start;
+					padding-left: 24px;
+				}
+			}
+			.main{
+				background: #ECEFF8;
+				height: 100%;
+				.el_card{
+					position: relative;
+					height: 100%;
+					overflow-y: scroll;
+				}
+				.width_style{
+					width: 1680px;
+				}
+				.el_card::-webkit-scrollbar {
+					display: none; 
+				}
+				.welcome_icon{
 					width: 100%;
 					height: 100%;
 				}
-				.num{
-					position: absolute;
-					top: -10px;
-					right: -18px;
-					border-radius: 8px;
-					background: red;
-					width: 24px;
-					text-align: center;
-					height: 16px;
-					line-height: 16px;
-					font-size: 8px;
-					color: #fff;
-				}
-			}
-			.admin_logo{
-				margin-right: 8px;
-				width: 21px;
-				height: 21px;
-			}
-			.header_text{
-				font-size: 14px;
-				color: #000;
 			}
 		}
 	}
-	.is-active {
-		background:#008DFF !important;
-	}
-	.el-menu{
-		border: none !important;
-	}
-	.content_box{
-		height: 600px;
-		.left_box{
-			background: #363D66;
-			.dd{
-				height: 35px;
-				display: flex;
-				align-items: flex-end;
-				justify-content: flex-end;
-				padding-right: 18px;
-				.collapse_icon{
-					color: #fff;
-					font-size:20px;
-				}
-			}
-			.start_dd{
-				justify-content: flex-start;
-				padding-left: 24px;
-			}
-		}
-		.main{
-			background: #ECEFF8;
-			height: 100%;
-			.el_card{
-				position: relative;
-				height: 100%;
-				overflow-y: scroll;
-			}
-			.width_style{
-				width: 1680px;
-			}
-			.el_card::-webkit-scrollbar {
-				display: none; 
-			}
-			.welcome_icon{
-				width: 100%;
-				height: 100%;
-			}
-		}
-	}
-}
 </style>
 <style type="text/css">
-.btn-wrapper-simple{
-	height: 24px !important;
-}
+	.btn-wrapper-simple{
+		height: 24px !important;
+	}
 </style>
 <script>
 	import watermark from '../api/watermark.js'
@@ -271,65 +271,84 @@
 				contextMenuData: {
 					menuName: 'demo',
          			//菜单显示的位置
-         			axis: {
-         				x: null,
-         				y: null
-         			},
+					axis: {
+						x: null,
+						y: null
+					},
          			//菜单选项
-         			menulists: [{
-         				fnHandler: 'home', 
-         				btnName: '打开新窗口' 
-         			}]
-         		},
-         		breadcrumb_list:[],
-         		is_phone:false
-         	}
-         },
-         created(){
+					menulists: [{
+						fnHandler: 'home', 
+						btnName: '打开新窗口' 
+					}]
+				},
+				breadcrumb_list:[],
+				is_phone:false
+			}
+		},
+		created(){
          	//判断是否是移动端
-         	if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
-         		this.is_phone = true;
-         	}
+			if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+				this.is_phone = true;
+			}
 			if(!this.$store.state.is_ding_talk){  //浏览器
         		//获取浏览器用户信息()
-        		this.GetUserInfo();
+				this.GetUserInfo();
       		}else{  //钉钉
          		//获取code
-         		this.GetCode();
-         	}
-         },
-         computed:{
-         	menu_list(){
-         		return this.$store.state.menu_list;
-         	}
-         },
-         watch:{
-         	$route(n){
-         		if(n.path != '/'){
-         			this.show_welcome = false;
-         			if(n.path == '/data_management' || n.path == '/commit_data'){
-         				this.activeIndex = '/data_fill';
-         			}
-         		};
-         	}
-         },
-         methods:{
+      			this.GetCode();
+      		}
+      	},
+      	mounted(){
+      		this.getUserIP();
+      	},
+      	computed:{
+      		menu_list(){
+      			return this.$store.state.menu_list;
+      		}
+      	},
+      	watch:{
+      		$route(n){
+      			if(n.path != '/'){
+      				this.show_welcome = false;
+      				if(n.path == '/data_management' || n.path == '/commit_data'){
+      					this.activeIndex = '/data_fill';
+      				}
+      			};
+      		}
+      	},
+      	methods:{
+      		getUserIP() {
+      			if (window.navigator.connection) {
+        			// 使用connection属性获取网络连接信息，适用于现代浏览器
+      				let ipAddress = window.navigator.connection.localAddress;
+      				console.log(ipAddress)
+      			} else if (window.navigator.getStats) {
+        			// 使用getStats方法获取网络连接信息，适用于旧版浏览器
+      				window.navigator.getStats(function(stats) {
+      					let ipAddress = stats.localAddress;
+      					console.log(ipAddress)
+      				}.bind(this));
+      			} else {
+      				let ipAddress = "无法获取IP地址";
+      				console.log(ipAddress)
+      			}
+      		},
 			//获取code
-			GetCode(){
-				dd.ready(() => {
-					dd.runtime.permission.requestAuthCode({
-						corpId: "ding7828fff434921f5b",
-						onSuccess: res =>{
+      		GetCode(){
+      			dd.ready(() => {
+      				dd.runtime.permission.requestAuthCode({
+      					corpId: "ding7828fff434921f5b",
+      					onSuccess: res =>{
                 			//获取钉钉用户信息
-                			let code = res.code;
-                			this.GetDingUserInfo(code);
-                		},
-                		onFail : err => {
-                			alert('dd error: ' + JSON.stringify(err));
-                		}
-                	});
-				});
-			},
+      						let code = res.code;
+      						this.GetDingUserInfo(code);
+      					},
+      					onFail : err => {
+      						alert('dd error: ' + JSON.stringify(err));
+      					}
+      				});
+      			});
+      		},
       		//钉钉获取用户信息
       		GetDingUserInfo(code){
       			resource.dingLogin({code:code}).then(res => {
@@ -362,96 +381,96 @@
       			//未读消息数
       			this.unreadnum();
 				//获取菜单列表
-				this.getMenuList();
-			},
+      			this.getMenuList();
+      		},
 			//未读消息数
-			unreadnum(){
-				resource.unreadnum().then(res => {
-					if(res.data.code == 1){
-						this.readnum = res.data.data;
-					}else{
-						this.$message.warning(res.data.msg);
-					}
-				})
-			},
+      		unreadnum(){
+      			resource.unreadnum().then(res => {
+      				if(res.data.code == 1){
+      					this.readnum = res.data.data;
+      				}else{
+      					this.$message.warning(res.data.msg);
+      				}
+      			})
+      		},
 			//获取消息列表
-			getList(){
-				resource.messageList(this.message_req).then(res => {
-					if(res.data.code == 1){
-						this.messageObj = res.data.data;
-					}else{
-						this.$message.warning(res.data.msg);
-					}
-				})
-			},
-			pageChange(val) {
-				this.message_req.page = val;
+      		getList(){
+      			resource.messageList(this.message_req).then(res => {
+      				if(res.data.code == 1){
+      					this.messageObj = res.data.data;
+      				}else{
+      					this.$message.warning(res.data.msg);
+      				}
+      			})
+      		},
+      		pageChange(val) {
+      			this.message_req.page = val;
 				//获取列表
-				this.getList();
-			},
+      			this.getList();
+      		},
 			//跳转消息详情
-			messageDetail(row){
-				if(row.status == 0){
-					resource.messageRead({id:row.message_id}).then(res => {
-						if(res.data.code == 1){
+      		messageDetail(row){
+      			if(row.status == 0){
+      				resource.messageRead({id:row.message_id}).then(res => {
+      					if(res.data.code == 1){
 								//未读消息数
-								this.unreadnum();
-							}else{
-								this.$message.warning(res.data.msg);
-							}
-						})
-				}
-				this.$router.push('/' + row.web_url);
-			},
+      						this.unreadnum();
+      					}else{
+      						this.$message.warning(res.data.msg);
+      					}
+      				})
+      			}
+      			this.$router.push('/' + row.web_url);
+      		},
 			//获取菜单列表
-			getMenuList(){
-				resource.getMenu().then(res => {
-					if(res.data.code == 1){
-						let menu_list = res.data.data;
-						this.$store.commit('menuList',menu_list);
-						var query = this.$route.query;
-						if(!!query.level2_url){
-							this.activeIndex = `/${query.level2_url}`;
-							this.$router.push(`/${query.level2_url}`);
-							this.$store.commit('currentTab',query.level3_url);
-						}else{
-							if(this.$route.path != '/'){
-								this.show_welcome = false;
-							}
+      		getMenuList(){
+      			resource.getMenu().then(res => {
+      				if(res.data.code == 1){
+      					let menu_list = res.data.data;
+      					this.$store.commit('menuList',menu_list);
+      					var query = this.$route.query;
+      					if(!!query.level2_url){
+      						this.activeIndex = `/${query.level2_url}`;
+      						this.$router.push(`/${query.level2_url}`);
+      						this.$store.commit('currentTab',query.level3_url);
+      					}else{
+      						if(this.$route.path != '/'){
+      							this.show_welcome = false;
+      						}
 							//数据填报-数据管理
-							if(this.$route.path == '/data_management' || this.$route.path == 'commit_data'){
-								this.activeIndex = '/data_fill';
-							}else{
-								this.activeIndex = `${this.$route.path}`;
-							}
-							var query_list = [];
-							for(let k in query){
-								let s = k + '=' + query[k];
-								query_list.push(s);
-							}
-							if(query_list.length > 0){
-								this.$router.push(`${this.$route.path}?${query_list.join('&')}`);
-							}else{
-								this.$router.push(`${this.$route.path}`);
-							}
-						}
-					}else{
-						this.$message.warning(res.data.msg);
-					}
-				})
-			},
-			showMenu () {
-				event.preventDefault();
-				var x = event.clientX;
-				var y = event.clientY;
-				this.contextMenuData.axis = {
-					x, y
-				};
-			},
+      						if(this.$route.path == '/data_management' || this.$route.path == 'commit_data'){
+      							this.activeIndex = '/data_fill';
+      						}else{
+      							this.activeIndex = `${this.$route.path}`;
+      						}
+      						var query_list = [];
+      						for(let k in query){
+      							let s = k + '=' + query[k];
+      							query_list.push(s);
+      						}
+      						if(query_list.length > 0){
+      							this.$router.push(`${this.$route.path}?${query_list.join('&')}`);
+      						}else{
+      							this.$router.push(`${this.$route.path}`);
+      						}
+      					}
+      				}else{
+      					this.$message.warning(res.data.msg);
+      				}
+      			})
+      		},
+      		showMenu () {
+      			event.preventDefault();
+      			var x = event.clientX;
+      			var y = event.clientY;
+      			this.contextMenuData.axis = {
+      				x, y
+      			};
+      		},
 			//打开新窗口
-			newWindow(){
-				var level2_url = window.location.hash.split('/')[1];
-				var level3_url = this.$store.state.current_tab;
+      		newWindow(){
+      			var level2_url = window.location.hash.split('/')[1];
+      			var level3_url = this.$store.state.current_tab;
 				if(!this.$store.state.is_ding_talk){  //浏览器
 					window.open(`${window.location.href}?level2_url=${level2_url}&level3_url=${level3_url}`);
 				}else{	//钉钉
@@ -460,13 +479,13 @@
 							corpId: "ding7828fff434921f5b",
 							onSuccess: res =>{
                			 		//获取钉钉用户信息
-               			 		let code = res.code;
-               			 		window.open(`${location.origin}/code_login?code=${code}&level2_url=${level2_url}&level3_url=${level3_url}`);
-               			 	},
-               			 	onFail : err => {
-               			 		alert('dd error: ' + JSON.stringify(err));
-               			 	}
-               			 });
+								let code = res.code;
+								window.open(`${location.origin}/code_login?code=${code}&level2_url=${level2_url}&level3_url=${level3_url}`);
+							},
+							onFail : err => {
+								alert('dd error: ' + JSON.stringify(err));
+							}
+						});
 					});
 				}
 			},
