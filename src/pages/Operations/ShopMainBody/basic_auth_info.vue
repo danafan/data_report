50726d281d44ac629a0e585929285b66806d965b
danafan <13067882143@163.com>
2023-12-14 09:40:51 +0800

    文件创建完成

---
+++ src/pages/Operations/ShopMainBody/basic_auth_info.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>店铺基础授权资料</div>
+</template>
+<script>
+    export default{
+
+    }
+</script>
+<style lang="less" scoped>
+    
+</style>
---
+++ src/pages/Operations/ShopMainBody/main_body_info.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>主题资料表</div>
+</template>
+<script>
+	export default{
+
+	}
+</script>
+<style lang="less" scoped>
+	
+</style>
